19227e991d06c21f8b4903aaddeb55007039a750
Samir Gossain <samirgossain@ELSEJetfire.local>
2022-04-20 17:00:51 +0100

    Makes note to re-adjust work so far with Attention to MUI V4 rather than V5 docs

--- src/components/PreJoinScreens/RoomNameScreen/RoomNameScreen.tsx
+++ src/components/PreJoinScreens/RoomNameScreen/RoomNameScreen.tsx
@@ -1,6 +1,20 @@
 import React, { ChangeEvent, FormEvent } from 'react';
-import { Typography, makeStyles, TextField, Grid, Button, InputLabel, Theme, Switch } from '@material-ui/core';
 import { useAppState } from '../../../state';
+import {
+  Box, 
+  Button, 
+  FormControl,
+  Grid, 
+  InputLabel, 
+  makeStyles, 
+  MenuItem,
+  Select,
+  Switch,
+  TextField, 
+  Theme, 
+  Typography
+} from '@material-ui/core';
+
 
 const useStyles = makeStyles((theme: Theme) => ({
   gutterBottom: {
@@ -9,7 +23,7 @@ const useStyles = makeStyles((theme: Theme) => ({
   inputContainer: {
     display: 'flex',
     justifyContent: 'space-between',
-    margin: '1.5em 0 3.5em',
+    margin: '1.5em 0 2.5em',
     '& div:not(:last-child)': {
       marginRight: '1em',
     },
@@ -42,9 +56,9 @@ export default function RoomNameScreen({ name, roomName, setName, setRoomName, h
   const classes = useStyles();
   const { user } = useAppState();
   // 
-  const [checked, setChecked] = React.useState(false); // change name to be more descriptive
+  const [checked, setChecked] = React.useState(false); // change name to be more descriptive: setDurationAndAgenda
 
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => { // change name to be more descriptive
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => { // change name to be more descriptive: handleSettingDurationAndAgenda
     console.log('Switch Changed', event.target.checked);
     setChecked(event.target.checked);
   };
@@ -103,20 +117,36 @@ export default function RoomNameScreen({ name, roomName, setName, setRoomName, h
           </div>
         </div>
 
-        {/*  */}
+        {/*  */}{/*  */}
+        {/* REDO THE FOLLOWING WITH ATTENTION TO V4 DOCUMENTATION! */}
         <div>
           <Typography className={classes.textFieldAndSwitch} variant="body1">Set Meeting Duration & Agenda?</Typography>
           <Switch checked={checked} onChange={handleChange} />
         </div>
 
-        {/* <p>{checked ? <span>TRUE</span> : <span>FALSE</span>}</p> */}
+        {checked ? (
+          <Box sx={{ width: 120 }}>
+            <FormControl fullWidth>
+              <InputLabel id="demo-simple-select-label">Duration</InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                label="Duration"
+                // value={age}
+                // onChange={handleChange}
+              >
+                <MenuItem value={10}>Ten</MenuItem>
+                <MenuItem value={20}>Twenty</MenuItem>
+                <MenuItem value={30}>Thirty</MenuItem>
+              </Select>
+            </FormControl>
+          </Box>
+        ) : null}
 
         {/* Input Label and Text Field for Room Duration */}
-
         {/* Dynamic List for Agenda Items: description|duration|order */}
         {/* 2 Buttons to Add/Remove Agenda Items to/from List  */}
-
-        {/*  */}
+        {/*  */}{/*  */}
 
         <Grid container justifyContent="flex-end">
           <Button
