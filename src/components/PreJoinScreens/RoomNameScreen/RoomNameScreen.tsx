f605b1dda8fafea2f68a78f900c276e820a057e2
Samir Gossain <samirgossain@elsejetfire.mynet>
2022-04-21 17:39:22 +0100

    Adds dummy interactions and logging for adding and removing agenda items

--- src/components/PreJoinScreens/RoomNameScreen/RoomNameScreen.tsx
+++ src/components/PreJoinScreens/RoomNameScreen/RoomNameScreen.tsx
@@ -1,17 +1,23 @@
 import React, { ChangeEvent, FormEvent } from 'react';
 import { useAppState } from '../../../state';
 import AddCircleIcon from '@material-ui/icons/AddCircle';
-import RemoveCircleIcon from '@material-ui/icons/RemoveCircle';
+import CreateIcon from '@material-ui/icons/Create';
+import DeleteIcon from '@material-ui/icons/Delete';
 import {
+  Avatar,
   Button, 
   Checkbox,
   IconButton,
   FormControl,
   FormControlLabel,
   InputLabel, 
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+  ListItemSecondaryAction,
   makeStyles, 
   MenuItem,
-  Select,
   TextField, 
   Theme, 
   Typography,
@@ -25,7 +31,7 @@ const useStyles = makeStyles((theme: Theme) => ({
   inputContainer: {
     display: 'flex',
     justifyContent: 'space-between',
-    margin: '1.5em 0 2.5em',
+    margin: '1.5em 0 3.5em',
     '& div:not(:last-child)': {
       marginRight: '1em',
     },
@@ -38,8 +44,6 @@ const useStyles = makeStyles((theme: Theme) => ({
   },
   durationCheckboxAndSelectContainer: {
     marginBottom: "3em",
-    display: 'flex',
-    flexDirection: 'row',
   },
   continueButton: {
     position: 'absolute',
@@ -55,6 +59,7 @@ const useStyles = makeStyles((theme: Theme) => ({
   formControl: {
     margin: theme.spacing(1),
     minWidth: 120,
+    bottom: 22
   },
   formControlLabel: {
     margin: '0',
@@ -63,6 +68,15 @@ const useStyles = makeStyles((theme: Theme) => ({
   agendaItemForm: {
     display: 'flex',
     flexDirection: 'row',
+  },
+  addAgendaItemButton: {
+    bottom: 12
+  },
+  removeAgendaItemButton: {
+    bottom: 12
+  },
+  agendaItemList: {
+    paddingTop: 0
   }
 }));
 
@@ -77,11 +91,16 @@ interface RoomNameScreenProps {
 export default function RoomNameScreen({ name, roomName, setName, setRoomName, handleSubmit }: RoomNameScreenProps) {
   const classes = useStyles();
   const { user } = useAppState();
-  const [checked, setChecked] = React.useState(false); // change name to be more descriptive
+  // 
+  const [chosenToSetDurationAndAgendaItems, setChosenToSetDurationAndAgendaItems] = React.useState(false); // change name to be more descriptive
+  const [dense, setDense] = React.useState(false);
+  const [secondary, setSecondary] = React.useState(false);
+  const [duration, setDuration] = React.useState(0)
+  const [agendaItems, setAgendaItems] = React.useState(0);
+  // 
 
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => { // change name to be more descriptive
-    console.log('Switch Changed', event.target.checked);
-    setChecked(event.target.checked);
+  const chooseToSetDurationAndAgendaItems = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setChosenToSetDurationAndAgendaItems(event.target.checked);
   };
 
   const handleNameChange = (event: ChangeEvent<HTMLInputElement>) => {
@@ -90,6 +109,24 @@ export default function RoomNameScreen({ name, roomName, setName, setRoomName, h
 
   const handleRoomNameChange = (event: ChangeEvent<HTMLInputElement>) => {
     setRoomName(event.target.value);
+  };
+
+  const handleDurationChange = (event: ChangeEvent<HTMLInputElement>) => {
+    console.log('Chosen Duration', event.target.value);
+    setDuration(parseInt(event.target.value));
+  };
+
+  const handleAgendaItemChange = () => {
+    console.log('AgendaItems:', agendaItems + 1);
+    setAgendaItems(agendaItems+1);
+  };
+
+  const generate = (element: React.ReactElement) => {
+    return [0, 1, 2].map((value) =>
+      React.cloneElement(element, {
+        key: value,
+      }),
+    );
   };
 
   const hasUsername = !window.location.search.includes('customIdentity=true') && user?.displayName;
@@ -140,55 +177,83 @@ export default function RoomNameScreen({ name, roomName, setName, setRoomName, h
         <div className={classes.durationCheckboxAndSelectContainer}>
           <FormControlLabel
             className={classes.formControlLabel}
-            control={<Checkbox checked={checked} onChange={handleChange} name="checkedA" />}
+            control={<Checkbox checked={chosenToSetDurationAndAgendaItems} onChange={chooseToSetDurationAndAgendaItems} name="checkedA" />}
             label="Set Duration"
             labelPlacement="start"
           />
 
-          {checked ? (
-            <FormControl className={classes.formControl}>
-              <InputLabel shrink htmlFor="input-room-duration">
-                Duration
-              </InputLabel>
-              <Select
-                labelId="demo-simple-select-label"
-                id="demo-simple-select"
-                // value={age}
-                // onChange={handleChange}
+          {chosenToSetDurationAndAgendaItems ? (
+            <FormControl variant="outlined" className={classes.formControl}>
+              <TextField
+                value={duration}
+                onChange={handleDurationChange}
+                select
+                label="Duration"
               >
                 <MenuItem value={15}>15</MenuItem>
                 <MenuItem value={30}>30</MenuItem>
                 <MenuItem value={45}>45</MenuItem>
                 <MenuItem value={60}>60</MenuItem>
-              </Select>
+              </TextField>
             </FormControl>
           ) : null}
         </div>
 
-        {checked ? (
-          <div className={classes.agendaItemForm}>
-            <Typography variant="body1">Press the +/- Button to Add/Remove Agenda Points</Typography>
-            <IconButton 
-              color="primary"
-              aria-label="add agenda item"
-              onClick={() => { console.log('add agenda item'); }}
-            >
-              <AddCircleIcon />
-            </IconButton>
-            <IconButton
-              color="primary"
-              aria-label="remove agenda item"
-              onClick={() => { console.log('remove agenda item'); }}
-            >
-              <RemoveCircleIcon />
-            </IconButton>
+        {chosenToSetDurationAndAgendaItems ? (
+          <div>
+            <div className={classes.agendaItemForm}>
+              <Typography variant="body1">Press the + Button to Add Agenda Points</Typography>
+              <IconButton 
+                color="primary"
+                aria-label="add agenda item"
+                onClick={handleAgendaItemChange}
+                className={classes.addAgendaItemButton}
+              >
+                <AddCircleIcon />
+              </IconButton>
+            </div>
+            <div>
+              <List className={classes.agendaItemList} dense={dense}>
+                {/* {Array(agendaItems).map(() => {
+                  (<ListItem>
+                    <ListItemAvatar>
+                      <Avatar>
+                        <CreateIcon />
+                      </Avatar>
+                    </ListItemAvatar>
+                    <ListItemText
+                      primary="Agenda Item"
+                      secondary={secondary ? 'Secondary text' : null}
+                    />
+                    <ListItemSecondaryAction>
+                      <IconButton edge="end" aria-label="delete" onClick={() => { console.log('remove agenda item'); }}>
+                        <DeleteIcon />
+                      </IconButton>
+                    </ListItemSecondaryAction>
+                  </ListItem>)
+                })} */}
+                {generate(
+                  <ListItem>
+                    <ListItemAvatar>
+                      <Avatar>
+                        <CreateIcon />
+                      </Avatar>
+                    </ListItemAvatar>
+                    <ListItemText
+                      primary="Agenda Item"
+                      secondary={secondary ? 'Secondary text' : null}
+                    />
+                    <ListItemSecondaryAction>
+                      <IconButton edge="end" aria-label="delete" onClick={() => { console.log('remove agenda item'); }}>
+                        <DeleteIcon />
+                      </IconButton>
+                    </ListItemSecondaryAction>
+                  </ListItem>,
+                )}
+              </List>
+            </div>
           </div>
         ) : null}
-
-        {/* AGENDA ITEMS & DURATION */}{/*  */}
-        {/* Dynamic List for Agenda Items: description|duration|order */}
-        {/* 2 Buttons to Add/Remove Agenda Items to/from List  */}
-        
 
         <Button
           variant="contained"
