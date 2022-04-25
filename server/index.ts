dbd6ec8a4c4e5e417335381c0833ce6b2a61024c
sim-ware <samir.gossain@gmail.com>
2022-04-25 17:31:15 +0100

    Scaffolds two routes for storage and fetching of Agenda Details

--- server/index.ts
+++ server/index.ts
@@ -25,6 +25,16 @@ const authMiddleware =
 app.all('/token', authMiddleware, tokenEndpoint);
 app.all('/recordingrules', authMiddleware, recordingRulesEndpoint);
 
+// 
+app.get('/saveMeetingAgendaDetails', (req, res) => {
+  res.send('samir is saving meeting agenda details');
+})
+
+app.get('/fetchMeetingAgendaDetails', (req, res) => {
+  res.send('samir is fetching meeting agenda details');
+})
+// 
+
 app.use((req, res, next) => {
   // Here we add Cache-Control headers in accordance with the create-react-app best practices.
   // See: https://create-react-app.dev/docs/production-build/#static-file-caching
@@ -39,10 +49,10 @@ app.use((req, res, next) => {
 
 app.use(express.static(path.join(__dirname, '../build')));
 
-app.get('*', (_, res) => {
-  // Don't cache index.html
-  res.set('Cache-Control', 'no-cache');
-  res.sendFile(path.join(__dirname, '../build/index.html'), { etag: false, lastModified: false });
-});
+// app.get('*', (_, res) => {
+//   // Don't cache index.html
+//   res.set('Cache-Control', 'no-cache');
+//   res.sendFile(path.join(__dirname, '../build/index.html'), { etag: false, lastModified: false });
+// });
 
 app.listen(PORT, () => console.log(`twilio-video-app-react server running on ${PORT}`));
